ce5830f7c2e40cf0f23d3a8cb4fb59106ec3bf76
Mindexperiment <andrewloyd@libero.it>
2019-09-07 22:45:15 +0200

    add metadata shortcut access

--- src/HasWing.php
+++ src/HasWing.php
@@ -8,11 +8,22 @@ namespace Agpretto\Wing;
 trait HasWing
 {
     /**
+     * Get the model wing relation
      * 
      * @return \Illuminate\Database\Eloquent\Relations\MorphOne
      */
     public function wing()
     {
         return $this->morphOne(Wing::class, 'model');
+    }
+
+    /**
+     * Get the model metadata
+     * 
+     * @return stdObject
+     */
+    public function metadata()
+    {
+        return $this->wing()->metadata;
     }
 }
