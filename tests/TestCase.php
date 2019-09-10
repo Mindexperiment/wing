6b2ebdd8348f2746d6d6b12d91efea7dac249004
Mindexperiment <andrewloyd@libero.it>
2019-09-10 20:45:16 +0200

    conform setup method to orchestra setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,7 +11,7 @@ abstract class TestCase extends OrchestraTestCase
         return ['Agpretto\Wing\WingServiceProvider'];
     }
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
